498f9fd8247431f2c2eb0eb2561065db364710b5
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-06 18:15:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(num)
   end
   return "Happy new year!".upcase
 end
+
+def countdown_with_sleep
+  while num != 0
+    puts "#{num} Second(s)!".upcase
+    num -= 1
+    sleep 1000
+  end
+  return "Happy new year!".upcase
+end
